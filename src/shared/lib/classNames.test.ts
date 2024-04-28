1a3fca7cf56bc3071a139437df2d42f9854690a9
Stanislav Dashkevitch <stasdaskevic6@gmail.com>
2024-04-28 17:14:25 +0300

    jest enviroment and add classNames test

---
+++ src/shared/lib/classNames.test.ts
@@ -0,0 +1,23 @@
+import { classNames } from "./classNames"
+
+describe('classname', () => {
+  test('with only 1 params', () => {
+    expect(classNames(('kuku'))).toBe('kuku')
+  })
+
+  test('with additional parmas', () => {
+    expect(classNames('kuku', {}, ['all', 'active'])).toBe('kuku all active')
+  })
+
+  test('with all params', () => {
+    expect(classNames('kuku', {"all": true}, ['all', 'active'])).toBe('kuku all active all')
+  })
+
+  test('with false params mods', () => {
+    expect(classNames('kuku', {"all": false}, ['all', 'active'])).toBe('kuku all active')
+  })
+
+  test('without params', () => {
+    expect(classNames()).toBe('')
+  })
+})
